764ebae576193d918caad00f83146ce1f8b2aa7f
Gowtham <gowtham.r64@wipro.com>
2017-01-20 17:04:13 +0530

    Dialog box for filtering notifications

--- popup.jsx
+++ popup.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import ajax from 'superagent';
-import Menu from './menus.jsx';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+let channelMenu=null;
 export default class Popup extends React.Component{
 	constructor(props) {
 		super(props);
@@ -12,22 +16,26 @@ export default class Popup extends React.Component{
 			status:false,
 			filterstatus:false,
 			filtertype:1,
-			projectName:0,
-			channelName:1,
-			tagName:1,
-			project:[]
+			projectValue:0,
+			channelValue:0,
+			tagValue:1,
+			project:[],
+			channel:[],
+			projectName:""
 
 		}
 		this.handleOpen=this.handleOpen.bind(this);
 		this.handleClose=this.handleClose.bind(this);
-		this.handleSecondOpen=this.handleSecondOpen.bind(this);
+		//this.handleSecondOpen=this.handleSecondOpen.bind(this);
 		this.handleSecondClose=this.handleSecondClose.bind(this);
 		this.handleChange=this.handleChange.bind(this);
 		this.handleProjectChange=this.handleProjectChange.bind(this);
 		this.handleChannelChange=this.handleChannelChange.bind(this);
 		this.handleTagChange=this.handleTagChange.bind(this);
-		this.getData=this.getData.bind(this);
-		//this.getProjectName=this.getProjectName.bind(this);
+		this.getProjectData=this.getProjectData.bind(this);
+		this.getChannelData=this.getChannelData.bind(this);
+		this.printChannelNames=this.printChannelNames.bind(this);
+		this.putFilterData=this.putFilterData.bind(this);
 	}
 	handleOpen(){
 		this.setState({status:true})
@@ -36,9 +44,9 @@ export default class Popup extends React.Component{
 		//console.log("handleClose");
 		this.setState({status:false})
 	}
-	handleSecondOpen(){
-		this.setState({filterstatus:true})
-	}
+	// handleSecondOpen(){
+	// 	this.setState({filterstatus:true})
+	// }
 	handleSecondClose(){
 		//console.log("handleClose");
 		this.setState({filterstatus:false})
@@ -46,56 +54,93 @@ export default class Popup extends React.Component{
 	handleChange(e,index,filtertype){
 		
 		this.setState({filtertype})
+		this.setState({filterstatus:true})
 		console.log(this.state.filtertype);
 	}
-	handleProjectChange(e,index,projectName){
+	handleProjectChange(e,index,projectValue){
 		console.log("clickkk");
-		this.setState({projectName})
+		this.setState({projectValue})
+		let that=this;
+		this.state.project.map(function(item,i){
+			if(i==that.state.projectValue){
+				// console.log("inside if");
+				// that.setState({projectName:item.id})
+				that.getChannelData(item.id);
+				
+			console.log("channelMenu",channelMenu);
+				
+			}
+		})
+		//console.log(that.state.projectName+"aaaa");
+		//console.log(that.state.projectName);
+
 	}
-	handleChannelChange(e,index,channelName){
-		this.setState({channelName})
+	handleChannelChange(e,index,channelValue){
+		this.setState({channelValue})
 	}
-	handleTagChange(e,index,tagName){
-		this.setState({tagName})
+	handleTagChange(e,index,tagValue){
+		this.setState({tagValue})
 	}
 	componentDidMount() {
-		this.getData();
+		this.getProjectData();
 	}
 
-	getData(){
+	getProjectData(){
 		let url="http://localhost:3002/Projects";
 		ajax.get(url).end((error,result)=>{
 			if(result){
 				this.setState({project:result.body})
 				console.log(result.body);
-				//this.putData();
+	}
+})
+	}
+	getChannelData(name){
+		console.log(name);
+		let url="http://localhost:3002/Projects/"+name;
+		ajax.get(url).end((error,result)=>{
+			if(result){
+				this.setState({channel:result.body.channels})
+				// console.log(result.body.channels);
+				// console.log("gettt",this.state.channel);
+				channelMenu=<SelectField floatingLabelText="SelectChannel" value={this.state.channelValue} onChange={this.handleChannelChange}>
+		    	{
+				this.state.channel.map(function(item,i){
+					return(<MenuItem key={i} value={i} primaryText={item.Name}/>)
+				})
+		    	}</SelectField>
+				this.printChannelNames();
 			}
-			else{
+			else
+			{
 				console.log(error);
 			}
 		})
 	}
 
+	putFilterData(){
+		console.log(this.state.projectValue);
+		console.log(this.state.channelValue);
+		console.log(this.state.tagValue);
+	}
+	printChannelNames(){
+		console.log(this.state.channel);
+	}
 	render(){
 		console.log("popup render");
 		var type=null;
-		const actions=[<FlatButton label="Cancel" onTouchTap={this.handleClose}/>,
-					<FlatButton label="Submit" onTouchTap={this.handleSecondOpen}/>]
-		const filterActions=[<FlatButton label="Cancel" onTouchTap={this.handleSecondClose}/>,
-					<FlatButton label="Submit" />]
+		console.log("inside redner",this.state);
+		const filterActions=[<RaisedButton label="Cancel" onTouchTap={this.handleSecondClose} secondary={true}/>,
+					<RaisedButton label="Submit" primary={true} onTouchTap={this.putFilterData} style={{marginLeft:"50px"}}/>]
 		if(this.state.filtertype==1)
 		{
 			type=<Dialog title="Filter By Project" actions={filterActions} modal={true} open={this.state.filterstatus} >
-			<SelectField floatingLabelText="SelectProject" value={this.state.projectName} onChange={this.handleProjectChange}>
+			<SelectField floatingLabelText="SelectProject" value={this.state.projectValue} onChange={this.handleProjectChange}>
 			{this.state.project.map(function(item,i){
 				return(<MenuItem key={i} value={i} primaryText={item.id}/>)
 			})}
 			</SelectField><br/>
-			<SelectField floatingLabelText="SelectChannel" value={this.state.channelName} onChange={this.handleChannelChange}>
-			<MenuItem value={1} primaryText="Developers"/>
-			<MenuItem value={2} primaryText="UX"/>
-			</SelectField><br/>
-			<SelectField floatingLabelText="SelectTag" value={this.state.tagName} onChange={this.handleTagChange}>
+			{channelMenu}<br/>
+			<SelectField floatingLabelText="SelectTag" value={this.state.tagValue} onChange={this.handleTagChange}>
 			<MenuItem value={1} primaryText="Important"/>
 			<MenuItem value={2} primaryText="Starred"/>
 			</SelectField></Dialog>
@@ -103,15 +148,27 @@ export default class Popup extends React.Component{
 		else
 		{
 			type=<Dialog title="Filter By Tags" actions={filterActions} modal={true} open={this.state.filterstatus}>
-			<SelectField floatingLabelText="SelectTag" value={this.state.tagName} onChange={this.handleTagChange}>
+			<SelectField floatingLabelText="SelectTag" value={this.state.tagValue} onChange={this.handleTagChange}>
 			<MenuItem value={1} primaryText="Important"/>
 			<MenuItem value={2} primaryText="Starred"/>
 			</SelectField></Dialog>
 		}
 		return(
 			<div>
-			<FlatButton label="Add Tile" onTouchTap={this.handleOpen}/>
-			<Dialog title="Filter By" actions={actions} modal={true} open={this.state.status}>
+			<Card>
+    <CardHeader
+      title="TITLE ADD"
+      subtitle="click the icon"
+    
+    />
+    <CardText>
+      <FloatingActionButton onTouchTap={this.handleOpen}>
+      <ContentAdd />
+    </FloatingActionButton>
+    </CardText>
+    
+  </Card>
+			<Dialog title="Filter By" modal={false} open={this.state.status} onRequestClose={this.handleClose}>
 			<SelectField floatingLabelText="select" value={this.state.filtertype} onChange={this.handleChange} style={{width:"300px"}}>
 			<MenuItem value={1} primaryText="Projects"/>
 			<MenuItem value={2} primaryText="Tags"/>
